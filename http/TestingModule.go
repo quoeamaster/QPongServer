2f44d0f23152b146db3a209185428f4fce356918
jason.wong <jason.wong@jasonwongs-MacBook-Pro.local>
2018-05-31 19:50:10 +0800

    2018-05-31a

---
+++ http/TestingModule.go
@@ -0,0 +1,16 @@
+package http
+
+/**
+ *  model for testing
+ */
+type TestingModel struct {
+	ArtistId string
+	ResourceName string     // resource name / identifier (unique within the Artist)
+	ResourceBytes []byte    // resource (binary) in []byte
+}
+
+func New *restful.
+
+
+
+
